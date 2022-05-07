9fe58f5b062cadcbfac699877cb72308ac997e3f
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-05-07 14:34:58 +0200

    :FEAT: => Ajout de la partie produit apres le fetch

--- src/components/Produits.tsx
+++ src/components/Produits.tsx
@@ -1,63 +1,96 @@
-import React, {useState, useEffect} from 'react';
-import Loading from './Loading';
-import ShowProducts from './ShowProducts';
-
-
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 const Produits = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
 
-    const [data, setData]         = useState([]);
-    const [filter, setFilter]     = useState(data);
-    const [loading, setLoading]   = useState(false);
-    
-    let componentMounted :boolean = true;
+  let componentMounted: boolean = true;
 
+  useEffect(() => {
+    const getProducts = async (value?: any) => {
+      setLoading(true);
 
+      const reponse = await fetch("https://fakestoreapi.com/products");
 
-    useEffect(() => {
+      if (componentMounted) {
+        setData(await reponse.clone().json());
+        setFilter(await reponse.json());
+        setLoading(false);
+      }
 
-        const getProducts = async (value? :any) => {
-            
-            setLoading(true);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      return () => (componentMounted = false);
+    };
 
-            const reponse = await fetch('https://fakestoreapi.com/products');
+    getProducts();
+  }, []);
 
-            if (componentMounted) {
-                setData(await reponse.clone().json());
-                setFilter(await reponse.json());
-                setLoading(false);
-            };
-
-            // eslint-disable-next-line react-hooks/exhaustive-deps
-            return () => componentMounted = false;
-            
-
-        };
-
-        getProducts();
-
-    }, []);
-    
-
-
+  const ShowProducts = () => {
     return (
-        <div>
-            <div className="container text-center py-2 arrow-container shadow-sm">
-               <i className="fa fa-arrow-down"></i> 
-            </div>
-            
-            <div className="container my-5 py-5">
-                <div className="row">
-                    <div className="col-12 mb-5">
-                        <h2 className='recent display-6 fw-bolder text-end'>Ajout récent</h2>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    {loading ? <Loading /> : <ShowProducts />}
-                </div>
-            </div>
+      <>
+        <div className="container buttons mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2">
+            Tous les produits
+          </button>
+          <button className="btn btn-outline-dark me-2">
+            Nouveauté pour lui
+          </button>
+          <button className="btn btn-outline-dark me-2">Biker style</button>
+          <button className="btn btn-outline-dark me-2">
+            Collection retro
+          </button>
         </div>
+        {filter.map((product?: any) => {
+          return (
+            <>
+              <div className="col-md-3">
+                <div className="card">
+                  <img
+                    src="https://dsdsd.fr"
+                    className="card-img-top"
+                    alt="produit"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="*" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
     );
+  };
+
+  return (
+    <div>
+      <div className="container text-center py-2 arrow-container shadow-sm">
+        <i className="fa fa-arrow-down"></i>
+      </div>
+
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h2 className="recent display-6 fw-bolder text-end">
+              Ajout récent
+            </h2>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Produits;
